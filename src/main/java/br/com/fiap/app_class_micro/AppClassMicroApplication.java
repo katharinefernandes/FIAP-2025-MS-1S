15df28c54fcbd34485dff2435bebe5222229aa46
katharinefernandes <katharinefv@gmail.com>
2025-09-02 22:40:50 -0300

    feat: configura projeto spring boot

---
+++ src/main/java/br/com/fiap/app_class_micro/AppClassMicroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.app_class_micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClassMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppClassMicroApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/fiap/app_class_micro/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.fiap.app_class_micro;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppClassMicroApplication.class);
+	}
+
+}
