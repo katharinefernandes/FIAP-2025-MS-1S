ed139a6b59f0e3c2965f4e7969a3e5087b12f0e1
katharinefernandes <katharinefv@gmail.com>
2025-09-16 22:37:58 -0300

    modifica entidade e controller

--- src/main/java/com/example/demo/model/Voucher.java
+++ src/main/java/com/example/demo/model/Voucher.java
@@ -1,20 +1,14 @@
 package com.example.demo.model;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class Voucher {
   private String code;
-  private Double pctDiscont;
-  private LocalDate validateDate;
   private VoucherTypeEnum type;
-  private Double minimumValue;
+  private BigDecimal value;
+  private BigDecimal minimumPurchase;
+  private Integer usageLimit;
+  private Integer usageCount;
   private LocalDate expirationDate;
 }
